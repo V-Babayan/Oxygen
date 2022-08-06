0ad10edc9fe32364c92e556325b5a2dab603e6df
m-avagyan <martikavagyan1@gmail.com>
2022-08-06 16:33:40 +0400

    deploy app

---
+++ app.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((request, response) => {
+  const path = __dirname + (request.url === '/' ? '/index.html' : request.url);
+
+  fs.readFile(path, (error, data) => {
+    if (error) {
+      response.writeHead(404);
+      response.end(JSON.stringify(err));
+    } else {
+      response.writeHead(200);
+      response.end(data);
+    }
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
