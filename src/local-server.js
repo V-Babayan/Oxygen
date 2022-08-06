60175729a9d941718e850660489435194febe530
m-avagyan <martikavagyan1@gmail.com>
2022-08-06 21:35:04 +0400

    feat: running app from local env

---
+++ src/local-server.js
@@ -0,0 +1,45 @@
+(function () {
+  const modes = {
+    PRODUCTION: 'production',
+    DEVELOPMENT: 'development',
+  };
+
+  const getCurrentMode = () => {
+    const hostname = window.location.hostname;
+
+    if (!hostname || hostname === 'localhost') {
+      return modes.DEVELOPMENT;
+    } else {
+      return modes.PRODUCTION;
+    }
+  };
+
+  const mode = getCurrentMode();
+
+  if (mode === modes.PRODUCTION) {
+    console.log('Please ignore all error messages');
+  } else {
+    // ! if you are created new script file, please do not forget add script file name to this array.
+    // ! this is a for local testing the app.
+    const SCRIPT_URLS = ['start'];
+
+    const elements = {
+      CSS: document.querySelector('#css-href-id'),
+      IMAGES: document.querySelectorAll('img'),
+    };
+
+    elements.CSS.href = elements.CSS.href.replace('src', '.');
+    elements.IMAGES.forEach((image) => {
+      image.src = image.src.replace('src', '.');
+    });
+
+    SCRIPT_URLS.forEach((url) => {
+      const script = document.createElement('script');
+
+      script.setAttribute('type', 'text/javascript');
+      script.setAttribute('src', './' + url + '.js');
+
+      document.body.appendChild(script);
+    });
+  }
+})();
